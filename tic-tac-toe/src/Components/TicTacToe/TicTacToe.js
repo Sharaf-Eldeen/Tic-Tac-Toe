0dfb57619ac5b1215fd4fe7b90029c836163d60f
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-09 21:47:05 +0300

    Add useRef To Reference Boxes and Title Of The Game

--- tic-tac-toe/src/Components/TicTacToe/TicTacToe.js
+++ tic-tac-toe/src/Components/TicTacToe/TicTacToe.js
@@ -1,9 +1,30 @@
 import "./TicTacToe.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TicTacToe() {
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
+  const titleRef = useRef(null);
+  const box1ref = useRef(null);
+  const box2ref = useRef(null);
+  const box3ref = useRef(null);
+  const box4ref = useRef(null);
+  const box5ref = useRef(null);
+  const box6ref = useRef(null);
+  const box7ref = useRef(null);
+  const box8ref = useRef(null);
+  const box9ref = useRef(null);
+  let boxsRef = [
+    box1ref,
+    box2ref,
+    box3ref,
+    box4ref,
+    box5ref,
+    box6ref,
+    box7ref,
+    box8ref,
+    box9ref,
+  ];
   return (
     <div className="container">
       <h1 className="title">
